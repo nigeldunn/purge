92d9f22c5b5a49f7219fb4baae489b1a26396a42
Nigel Dunn <nigel.dunn@gmail.com>
2022-06-02 22:22:27 +1000

    ADDED input scaffolding

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,42 @@
 use bevy::prelude::*;
+use leafwing_input_manager::prelude::*;
+use leafwing_input_manager::{errors::NearlySingularConversion, orientation::Direction};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_system(hello)
+        .add_plugin(InputManagerPlugin::<Action>::default())
+        .add_startup_system(spawn_player)
+        .add_system(jump)
         .run();
 }
 
-fn hello() {
-    println!("Hello, world!");
+#[derive(Actionlike, PartialEq, Eq, Clone, Copy, Hash, Debug)]
+enum Action {
+    Run,
+    Jump,
+}
+
+#[derive(Component)]
+struct Player;
+
+fn spawn_player(mut commands: Commands) {
+    commands
+        .spawn()
+        .insert(Player)
+        .insert_bundle(InputManagerBundle::<Action> {
+            // Stores "which actions are currently pressed"
+            action_state: ActionState::default(),
+            // Describes how to convert from player inputs into those actions
+            input_map: InputMap::new([(Action::Jump, KeyCode::Space)]),
+        });
+}
+
+// Query for the `ActionState` component in your game logic systems!
+fn jump(query: Query<&ActionState<Action>, With<Player>>) {
+    let action_state = query.single();
+    // Each action has a button-like state of its own that you can check
+    if action_state.just_pressed(Action::Jump) {
+        println!("I'm jumping!");
+    }
 }
