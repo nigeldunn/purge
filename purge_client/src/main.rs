1545a380e0d0b14b421436eb7ff7556c26afe23b
Nigel Dunn <nigel.dunn@gmail.com>
2022-06-06 22:52:16 +1000

    ADDED fast compiling and nightly tools

--- purge_client/src/main.rs
+++ purge_client/src/main.rs
@@ -1,5 +1,9 @@
 use purge_common;
+use bevy::prelude::*;
 
 fn main() {
     println!("{}", purge_common::hello_world("Purge".to_string()));
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .run();
 }
--- purge_server/src/main.rs
+++ purge_server/src/main.rs
@@ -1,5 +1,8 @@
 use purge_common;
+use bevy::prelude::*;
 
 fn main() {
     println!("{}", purge_common::hello_world("Purge".to_string()));
+    App::new()
+        .run();
 }
